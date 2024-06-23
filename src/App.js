8d919f74bf15dc75068c0ae2cd4e8dd57366d4a6
0MadGaming0 <madhavsp05@gmail.com>
2024-06-23 09:04:17 +0530

    Newspaper Subscription  App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AddSubscriber from './components/AddSubscriber';
 import ViewSubscriber from './components/ViewSubscriber';
+import SearchSubscriber from './components/SearchSubscriber';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path="/" element={<AddSubscriber/>}/>
       <Route path="/view" element={<ViewSubscriber/>}/>
+      <Route path="/search" element={<SearchSubscriber/>}/>
     </Routes>
     </BrowserRouter>
   );
